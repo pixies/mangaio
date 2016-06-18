2d377a8652959539c969bf948226e4e377a0f67b
Vyctor Oliveira <vyctor@tagi.com.br>
2016-06-18 18:56:02 -0300

    Adicionados os caminhos de MEDIA e STATIC

--- mangaio/urls.py
+++ mangaio/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from comunidade.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^$', index),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
