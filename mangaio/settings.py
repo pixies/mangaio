2b18fb301ddcd9016ff914a38cdf4e40017c6e2f
Vyctor Oliveira <vyctor@tagi.com.br>
2016-06-18 18:57:14 -0300

    Adicionadas MEDIA, STATIC e TEMPLATES

--- mangaio/settings.py
+++ mangaio/settings.py
@@ -121,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/vyctor/Projetos/mangaio/static'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/vyctor/Projetos/mangaio/media'
+
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),) 
