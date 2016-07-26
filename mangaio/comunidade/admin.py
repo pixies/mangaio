aab5b3c7ba04a31f8b7672d70a47efd0c0017b76
Cleyton Fábio <pixies@pelivre.org>
2016-07-26 09:36:54 -0300

    removendo app comunidade - repensando na ideia

--- mangaio/comunidade/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from mangaio.comunidade.models import Usuario,UserProfile,Endereco,RedeSocial
-
-# Register your models here.
-admin.site.register(Usuario)
-admin.site.register(UserProfile)
-admin.site.register(Endereco)
-admin.site.register(RedeSocial)
--- mangaio/comunidade/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from mangaio.comunidade.models import UserProfile
-
-# Create your views here.
-def index(request):
-	
-	profiles = UserProfile.objects.all()
-
-	return render(request, 'comunidade/index.html', {'profiles':profiles})
