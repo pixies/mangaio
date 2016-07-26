842f5abd74a48c7784ed2cd71646f93a01909b9c
Cleyton Fábio <pixies@pelivre.org>
2016-07-26 09:47:58 -0300

    Aplicação base para qualquer website

---
+++ mangaio/core/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+
+    context = {}
+    conteudos = [
+        {
+        'autor': 'Vyctor Olineira',
+        'categoria': 'Arocha',
+        'time': '3 dias atrás · leitura de 5 min ',
+        'conteudo': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis. ',
+        'like': '666'
+        },
+        {
+            'autor': 'Vyctor Olineira   ',
+            'categoria': 'Lambada',
+            'time': '4 dias atrás · leitura de 45 min ',
+            'conteudo': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis. ',
+            'like': '66'
+        }
+    ]
+    context['conteudos'] = conteudos
+    return render(request, 'index.html', context)
+
+def contact(request):
+    return render(request, 'contato.html')
